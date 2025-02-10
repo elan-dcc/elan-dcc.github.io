46f64cd7ed470ab7240b6cb80a562cc16f1c1953
Schipper <l.de_schipper@lumc.nl>
2025-02-10 16:17:33 +0100

    test

--- .github/scripts/genmmds.py
+++ .github/scripts/genmmds.py
@@ -80,6 +80,7 @@ for x in range(len(graphs_flowchart) + 1):
                 subchart = extract_subgraphs(subgraphs[0], graphs_flowchart[graph])
                 my_subgraphs.update(subchart)
                 cleaned_chart = re.sub(rf'{subgraphs[0]}\n(.*?)\nend', '', graphs_flowchart[graph], flags = re.DOTALL).strip()
+    	        print(cleaned_chart)
                 if cleaned_chart:
                     output.write(cleaned_chart + "\n")
             if subgraphs:
